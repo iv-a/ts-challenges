90070cb8306a1975afd1bc934bd09a35b9ddc67a
Artem Ivanov <ivart.dev@gmail.com>
2022-07-08 05:40:00 +0000

    Commit 00531-medium-string-to-union.ts

---
+++ 00531-medium-string-to-union/00531-medium-string-to-union.ts
@@ -0,0 +1,22 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<StringToUnion<''>, never>>,
+  Expect<Equal<StringToUnion<'t'>, 't'>>,
+  Expect<Equal<StringToUnion<'hello'>, 'h' | 'e' | 'l' | 'l' | 'o'>>,
+  Expect<Equal<StringToUnion<'coronavirus'>, 'c' | 'o' | 'r' | 'o' | 'n' | 'a' | 'v' | 'i' | 'r' | 'u' | 's'>>,
+]
+
+
+// ============= Your Code Here =============
+// type StringToUnion<T extends string, R extends unknown[] = []> = 
+//   T extends `${infer F}${infer L}` 
+//     ? StringToUnion<L, [...R, F]>
+//     : R[number]
+
+
+type StringToUnion<T extends string> = 
+  T extends `${infer F}${infer L}` 
+    ? F | StringToUnion<L> 
+    : never
