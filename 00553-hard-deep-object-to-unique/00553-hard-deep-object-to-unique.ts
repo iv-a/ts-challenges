73454d4f16e54cfb0604ef4d41ce5aa8794b51d0
Artem Ivanov <ivart.dev@gmail.com>
2022-07-11 08:40:00 +0000

    Commit 00553-hard-deep-object-to-unique.ts

---
+++ 00553-hard-deep-object-to-unique/00553-hard-deep-object-to-unique.ts
@@ -0,0 +1,43 @@
+// ============= Test Cases =============
+import type { Equal, IsFalse, IsTrue } from './test-utils'
+
+type Quz = { quz: 4 }
+
+type Foo = { foo: 2; baz: Quz; bar: Quz }
+type Bar = { foo: 2; baz: Quz; bar: Quz & { quzz?: 0 } }
+
+type UniqQuz = DeepObjectToUniq<Quz>
+type UniqFoo = DeepObjectToUniq<Foo>
+type UniqBar = DeepObjectToUniq<Bar>
+
+declare let foo: Foo
+declare let uniqFoo: UniqFoo
+
+uniqFoo = foo
+foo = uniqFoo
+
+type cases = [
+  IsFalse<Equal<UniqQuz, Quz>>,
+  IsFalse<Equal<UniqFoo, Foo>>,
+  IsTrue<Equal<UniqFoo['foo'], Foo['foo']>>,
+  IsTrue<Equal<UniqFoo['bar']['quz'], Foo['bar']['quz']>>,
+  IsFalse<Equal<UniqQuz, UniqFoo['baz']>>,
+  IsFalse<Equal<UniqFoo['bar'], UniqFoo['baz']>>,
+  IsFalse<Equal<UniqBar['baz'], UniqFoo['baz']>>,
+  IsTrue<Equal<keyof UniqBar['baz'], keyof UniqFoo['baz']>>,
+  IsTrue<Equal<keyof Foo, keyof UniqFoo & string>>,
+]
+
+
+// ============= Your Code Here =============
+type DeepObjectToUniq<
+  T extends object,
+  P extends unknown[] = []
+> = {
+  [K in keyof T]:
+    T[K] extends object 
+      ? DeepObjectToUniq<T[K], [T, K]>
+      : T[K]
+} & {
+  readonly [key: symbol]: P
+};
