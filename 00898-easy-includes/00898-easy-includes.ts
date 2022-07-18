e8fae9a5cfa9fe9593e7f81857383317f57dffd3
Artem Ivanov <ivart.dev@gmail.com>
2022-07-18 08:19:00 +0000

    Commit 00898-easy-includes.ts

---
+++ 00898-easy-includes/00898-easy-includes.ts
@@ -0,0 +1,36 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Kars'>, true>>,
+  Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>, false>>,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 7>, true>>,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
+  Expect<Equal<Includes<[1, 2, 3], 2>, true>>,
+  Expect<Equal<Includes<[1, 2, 3], 1>, true>>,
+  Expect<Equal<Includes<[{}], { a: 'A' }>, false>>,
+  Expect<Equal<Includes<[boolean, 2, 3, 5, 6, 7], false>, false>>,
+  Expect<Equal<Includes<[true, 2, 3, 5, 6, 7], boolean>, false>>,
+  Expect<Equal<Includes<[false, 2, 3, 5, 6, 7], false>, true>>,
+  Expect<Equal<Includes<[{ a: 'A' }], { readonly a: 'A' }>, false>>,
+  Expect<Equal<Includes<[{ readonly a: 'A' }], { a: 'A' }>, false>>,
+  Expect<Equal<Includes<[1], 1 | 2>, false>>,
+  Expect<Equal<Includes<[1 | 2], 1>, false>>,
+  Expect<Equal<Includes<[null], undefined>, false>>,
+  Expect<Equal<Includes<[undefined], null>, false>>,
+]
+
+
+// ============= Your Code Here =============
+// type isEqual<T, U> = (<T>() => T extends U ? 1 : 2) extends (<U>() => U extends T ? 1: 2) ? true : false;
+type isEqual<X, Y> = 
+  (<G>() => G extends X ? 1 : 2) extends
+  (<G>() => G extends Y ? 1 : 2) 
+    ? true
+    : false;
+
+type Includes<T extends readonly any[], U> = 
+{
+  [Key in keyof T]: isEqual<T[Key], U>
+}[number] extends false ? false : true;
+  
