894b00ffa8214c002e65a6ec64c4ab09e53f7602
Artem Ivanov <ivart.dev@gmail.com>
2022-08-02 05:05:00 +0000

    Commit 02757-medium-partialbykeys.ts

---
+++ 02757-medium-partialbykeys/02757-medium-partialbykeys.ts
@@ -0,0 +1,35 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+interface User {
+  name: string
+  age: number
+  address: string
+}
+
+interface UserPartialName {
+  name?: string
+  age: number
+  address: string
+}
+
+interface UserPartialNameAndAge {
+  name?: string
+  age?: number
+  address: string
+}
+
+type cases = [
+  Expect<Equal<PartialByKeys<User, 'name'>, UserPartialName>>,
+  Expect<Equal<PartialByKeys<User, 'name' | 'age'>, UserPartialNameAndAge>>,
+  Expect<Equal<PartialByKeys<User>, Partial<User>>>,
+  // @ts-expect-error
+  Expect<Equal<PartialByKeys<User, 'name' | 'unknown'>, UserPartialName>>,
+]
+
+
+// ============= Your Code Here =============
+type PartialByKeys<T extends object, K extends keyof T = keyof T> = 
+  Omit<Omit<T, K> & { 
+    [Key in K]?: T[Key] 
+  }, never>
