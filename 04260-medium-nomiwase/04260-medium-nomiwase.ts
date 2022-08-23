9a462dc07f8f7edbabf09fce5aef6dcc832eb6bf
Artem Ivanov <ivart.dev@gmail.com>
2022-08-23 05:16:00 +0000

    Commit 04260-medium-nomiwase.ts

---
+++ 04260-medium-nomiwase/04260-medium-nomiwase.ts
@@ -0,0 +1,30 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<AllCombinations<''>, ''>>,
+  Expect<Equal<AllCombinations<'A'>, '' | 'A'>>,
+  Expect<Equal<AllCombinations<'AB'>, '' | 'A' | 'B' | 'AB' | 'BA'>>,
+  Expect<Equal<AllCombinations<'ABC'>, '' | 'A' | 'B' | 'C' | 'AB' | 'AC' | 'BA' | 'BC' | 'CA' | 'CB' | 'ABC' | 'ACB' | 'BAC' | 'BCA' | 'CAB' | 'CBA'>>,
+  Expect<Equal<AllCombinations<'ABCD'>, '' | 'A' | 'B' | 'C' | 'D' | 'AB' | 'AC' | 'AD' | 'BA' | 'BC' | 'BD' | 'CA' | 'CB' | 'CD' | 'DA' | 'DB' | 'DC' | 'ABC' | 'ABD' | 'ACB' | 'ACD' | 'ADB' | 'ADC' | 'BAC' | 'BAD' | 'BCA' | 'BCD' | 'BDA' | 'BDC' | 'CAB' | 'CAD' | 'CBA' | 'CBD' | 'CDA' | 'CDB' | 'DAB' | 'DAC' | 'DBA' | 'DBC' | 'DCA' | 'DCB' | 'ABCD' | 'ABDC' | 'ACBD' | 'ACDB' | 'ADBC' | 'ADCB' | 'BACD' | 'BADC' | 'BCAD' | 'BCDA' | 'BDAC' | 'BDCA' | 'CABD' | 'CADB' | 'CBAD' | 'CBDA' | 'CDAB' | 'CDBA' | 'DABC' | 'DACB' | 'DBAC' | 'DBCA' | 'DCAB' | 'DCBA'>>,
+]
+
+
+// ============= Your Code Here =============
+type String2Union<S extends string> = 
+  S extends `${infer First}${infer Rest}`
+    ? First | String2Union<Rest>
+    : S;
+
+type AllCombinations<
+  S extends string, 
+  U extends string = String2Union<S>,
+  T extends string = U,
+> = 
+  T extends T
+    ? '' | `${T}${AllCombinations<S, Exclude<U, T>>}`
+    : never
+
+  ;
+
+type t1 = String2Union<'AB'>
