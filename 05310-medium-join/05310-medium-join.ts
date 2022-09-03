1be276a1a0a7f3be0f7bf9743aff20715c3c2308
Artem Ivanov <ivart.dev@gmail.com>
2022-09-03 09:23:00 +0000

    Commit 05310-medium-join.ts

---
+++ 05310-medium-join/05310-medium-join.ts
@@ -0,0 +1,23 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<Join<['a', 'p', 'p', 'l', 'e'], '-'>, 'a-p-p-l-e'>>,
+  Expect<Equal<Join<['Hello', 'World'], ' '>, 'Hello World'>>,
+  Expect<Equal<Join<['2', '2', '2'], 1>, '21212'>>,
+  Expect<Equal<Join<['o'], 'u'>, 'o'>>,
+]
+
+
+// ============= Your Code Here =============
+type Join<
+  T extends string[], 
+  U extends string | number
+> =
+  T extends [infer First extends string, ...infer Rest extends string[]]
+    ? `${First}${Rest extends [] 
+      ? '' 
+      : `${U}${Join<Rest, U>}`}`
+    : ''
+
+    type t1 = Join<['a', 'p', 'p', 'l', 'e'], '-'>
