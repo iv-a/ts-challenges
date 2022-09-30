6997fd3fed598c31b634b758585d016e51365c73
Artem Ivanov <ivart.dev@gmail.com>
2022-09-30 07:25:00 +0000

    Commit 18142-medium-all.ts

---
+++ 18142-medium-all/18142-medium-all.ts
@@ -0,0 +1,39 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<All<[1, 1, 1], 1>, true>>,
+  Expect<Equal<All<[1, 1, 2], 1>, false>>,
+  Expect<Equal<All<['1', '1', '1'], '1'>, true>>,
+  Expect<Equal<All<['1', '1', '1'], 1>, false>>,
+  Expect<Equal<All<[number, number, number], number>, true>>,
+  Expect<Equal<All<[number, number, string], number>, false>>,
+  Expect<Equal<All<[null, null, null], null>, true>>,
+  Expect<Equal<All<[[1], [1], [1]], [1]>, true>>,
+  Expect<Equal<All<[{}, {}, {}], {}>, true>>,
+  Expect<Equal<All<[never], never>, true>>,
+  Expect<Equal<All<[any], any>, true>>,
+  Expect<Equal<All<[unknown], unknown>, true>>,
+  Expect<Equal<All<[any], unknown>, false>>,
+  Expect<Equal<All<[unknown], any>, false>>,
+]
+
+
+// ============= Your Code Here =============
+// type All<
+//   A extends unknown[],
+//   T extends unknown
+// > = 
+//   A extends [infer F, ...infer R]
+//     ? Equal<F, T> | All<R, T> extends true
+//       ? true
+//       : false
+//     : 
+
+type All<
+  A extends unknown[],
+  T extends unknown
+> = 
+  Equal<A[number], T> extends true 
+    ? true
+    : false
